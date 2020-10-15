90d956e158e4233aaf8535c10d876f6457397fb6
Ugochukwu <dykeehenry@gmail.com>
2020-10-15 16:21:23 +0100

    set up mongo and mongoose

--- sever.js
+++ sever.js
@@ -2,11 +2,16 @@ const express = require('express');
 const dotenv = require('dotenv');
 //const { json } = require('express');
 const logger = require('./middleware/logger');
-const morgan  = require('morgan')
+const morgan  = require('morgan');
+ const connectdb =  require('./config/db');
 
 //load config file
-
 dotenv.config({ path: './config/config.env' });
+
+
+//connect to db
+connectdb();
+ 
 
 //Routes files
 const bootcamps = require('./routes/bootcamps');
@@ -25,4 +30,11 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`sever running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`sever running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+//handle unhandled rejection 
+
+process.on('unhandled rejection',(err, Promise) => {
+    console.log(`unhandled rejection: ${err.message}`);
+    server.close()
+})
